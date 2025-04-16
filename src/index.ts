831e5d2fca2b3506feeb7fd137c2a54e049ff40b
Kevin Dela Rosa <kevd1337@gmail.com>
2025-04-16 10:18:39 -0700

    testing shebang

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+console.log("Starting CloudGlue MCP Server");
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { CloudGlue } from "@aviaryhq/cloudglue-js";
